e78d75c3fa66431d055c77434429f632bc3fc029
Eric Urzedo <ericurzedo@gmail.com>
2025-08-05 07:27:01 -0300

    Teste

--- client/src/components/ShowBookList.jsx
+++ client/src/components/ShowBookList.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import "../styles.css";
 import { Link } from "react-router-dom";
-import DataTable from './DataTable';
+import DataTable from "DataTable";
 import API from "../API";
 
 function ShowBookList() {
